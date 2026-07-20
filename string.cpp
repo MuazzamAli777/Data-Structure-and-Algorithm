d4175ff490b0c7ab0f434ed2e4119d0db8804c7f
MuazzamAli777 <mozambhali521@gmail.com>
2026-07-20 06:53:34 -0700

    string methods

--- string.cpp
+++ string.cpp
@@ -5,6 +5,7 @@ int main (){
 string a="hello ,world ";
 cout<<"length of string "<<a.length()<<endl;
 cout<<"substring....:"<<a.substr(0,6)<<endl;//like slice 
+cout<<"find method at index:"<<a.find("hello")<<endl;
 
     cout<<"hello world ";
     return 0;
