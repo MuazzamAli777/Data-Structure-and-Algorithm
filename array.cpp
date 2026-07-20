1a3a8c46dee93f5835c1e5c4f478886f04299fce
MuazzamAli777 <mozambhali521@gmail.com>
2026-07-20 06:40:15 -0700

    DSA Learning

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+vector<int> ver={55,54,55};
+
+    cout<<"hello world ";
+    return 0;
+
+}
---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+string a="hello ,world ";
+cout<<"length of string "<<a.length()<<endl;
+cout<<"substring....:"<<a.substr(0,6)<<endl;//like slice 
+
+    cout<<"hello world ";
+    return 0;
+
+}
